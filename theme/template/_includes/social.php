e7f9fa87bc1d23f339e8b43e46dc74cf683d530c
Hayato Tomoda <tomodian@gmail.com>
2020-01-29 02:22:48 +0900

    Feature: Added tracker classes

--- theme/template/_includes/social.php
+++ theme/template/_includes/social.php
@@ -1,8 +1,8 @@
 <section>
-  <a class="icon icon--twitter" href="<?= tweet_url(
+  <a class="icon icon--twitter js-track-twitter-link" href="<?= tweet_url(
     $post
   ) ?>" target="_blank"></a>
-  <a class="icon icon--facebook" href="<?= facebook_url(
+  <a class="icon icon--facebook js-track-facebook-link" href="<?= facebook_url(
     $post
   ) ?>" target="_blank"></a>
 </section>
